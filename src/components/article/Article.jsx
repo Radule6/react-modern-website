eaab1383437e7cdec72f8be1e1293c1f8bafa72a
Marko Radulovic <marko.radulovic04@gmail.com>
2022-11-17 18:54:58 +0100

    cta blog complete

--- src/components/article/Article.jsx
+++ src/components/article/Article.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
-import "./article.css"
-const Article = () => {
-    return (
-        <div>Article</div>
-    )
-}
+import React from "react";
+import "./article.css";
 
-export default Article
+const Article = ({ imgSrc, date, title }) => {
+  return (
+    <div className="gpt3__blog-container__article">
+      <div className="gpt3__blog-container__article-image">
+        <img src={imgSrc} alt="blog" />
+      </div>
+      <div className="gpt3__blog-container__article-content">
+        <div>
+          <p>{date}</p>
+          <h3>{title}</h3>
+        </div>
+        <p>Read Full Article</p>
+      </div>
+    </div>
+  );
+};
+
+export default Article;
